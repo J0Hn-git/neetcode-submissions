9587060bae58c3e72fd2e9df7d0ae0e862377f44
JOHN JOJI <johnjoji2222@gmail.com>
2026-06-08 13:29:25 +0530

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if(nums.length == 0){
+            return false;
+        }
+        Set<Integer> seen = new HashSet<>();
+
+        for(int num : nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            else{
+                seen.add(num);
+            }
+        }
+        return false;
+    }
+}
---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int scoreOfString(String s) {
+        
+        int n = s.length();
+
+        int sum = 0;
+
+        for(int i = 0; i < n-1; i++){
+            
+            int a = (int)s.charAt(i);
+            int b = (int)s.charAt(i+1);
+
+            sum += Math.abs(a - b);
+
+        }
+        return sum;
+    }
+}
