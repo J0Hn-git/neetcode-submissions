e995ceec195e24f382675df9e7abc35d22b42ec3
JOHN JOJI <johnjoji2222@gmail.com>
2026-08-11 22:32:11 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        
+        Set<String> set = new HashSet<>();
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+
+                char current = board[i][j];
+
+                if(current != '.'){
+
+                    String rowKey = "row" + i + current;
+                    String colKey = "col" + j + current;
+                    String boxKey = "box" + (i/3) + (j/3) + current;
+
+                    if(!set.add(rowKey) || !set.add(colKey) || !set.add(boxKey)){
+                        return false;
+                    }
+            
+                }    
+            }
+        }
+        return true;
+    }
+}
