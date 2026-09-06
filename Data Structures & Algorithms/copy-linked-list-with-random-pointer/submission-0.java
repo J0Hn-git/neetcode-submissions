50991fd01874ce53fce91e7da0c511ddbeca1005
JOHN JOJI <johnjoji2222@gmail.com>
2026-09-06 23:21:59 +0530

    Update: copy-linked-list-with-random-pointer - submission-0

--- Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-0.java
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-0.java
@@ -1,31 +1,47 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
 
 class Solution {
-    Map<Node, Node> map = new HashMap<>();
     public Node copyRandomList(Node head) {
-        if(head == null) return null;
-        if(map.containsKey(head)) return map.get(head);
-
-        Node copy = new Node(head.val);
-        map.put(head, copy);
-
-        copy.next = copyRandomList(head.next);
-        copy.random = copyRandomList(head.random);
-
-        return copy;
         
+        if(head == null) {
+            return null;
+        }
+        ArrayList<Node> original = new ArrayList<>();
+        ArrayList<Node> copy = new ArrayList<>();
+
+        Node curr = head;
+
+        // Store original nodes and create copies.
+
+        while(curr != null) {
+
+            original.add(curr);
+            copy.add(new Node(curr.val));
+
+            curr = curr.next;
+        }
+        // Connect next Pointers.
+        for(int i = 0; i < copy.size() - 1; i++) {
+
+            copy.get(i).next = copy.get(i + 1);
+        }
+        // Connect random pointers.
+        for(int i = 0; i < original.size(); i++) {
+
+            Node randomNode = original.get(i).random;
+
+            if(randomNode == null) {
+                copy.get(i).random = null;
+                continue;
+            }
+            // Find the randomNode's index in original
+            for(int j = 0; j < original.size(); j++) {
+
+                if(original.get(j) == randomNode) {
+                    copy.get(i).random = copy.get(j);
+                    break;
+                }
+            }
+        }
+        return copy.get(0);
     }
 }
