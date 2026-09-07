f7de52c7715a75d5ffde9296c8775e6403d26345
JOHN JOJI <johnjoji2222@gmail.com>
2026-09-07 07:58:36 +0530

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.java
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -1,25 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // if the length are different, they can't be anagrams.
+
         if(s.length() != t.length()) {
             return false;
         }
+        int[] charArray = new int[26];
 
-        // create an array to count character frequencies.
-        int[] charCounts = new int[26];
+        for(int i = 0; i < s.length(); i++) {
 
-        // inc count for each char in 's' and dec for each in 't'.
-        for(int i=0;i<s.length();i++) {
-            charCounts[s.charAt(i) - 'a']++;
-            charCounts[t.charAt(i) - 'a']--;
+            charArray[s.charAt(i) - 'a']++;
+            charArray[t.charAt(i) - 'a']--;
         }
-        // check if all counts are zero
-        for(int count : charCounts) {
+        for(int count : charArray) {
             if(count != 0) {
                 return false;
             }
         }
         return true;
-
     }
 }
