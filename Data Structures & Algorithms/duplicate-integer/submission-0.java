0a86d77a5c6df5bf1e9c14f0dafa65aa3c6960df
JOHN JOJI <johnjoji2222@gmail.com>
2026-09-07 07:55:09 +0530

    Update: duplicate-integer - submission-0

--- Data Structures & Algorithms/duplicate-integer/submission-0.java
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-        // create a hashset to store elements from array.
-        HashSet<Integer> seenNumbers = new HashSet<>();
+        
+        Set<Integer> seenNumbers = new HashSet<>();
 
-        // Iterate through each element in array.
         for(int num : nums) {
-            // Check if the element is already in the hashset.
             if(seenNumbers.contains(num)) {
-                return true; // duplicate found.
+                return true;
             }
-            // add the element to the hashset
-            seenNumbers.add(num);
-        } 
+            else {
+                seenNumbers.add(num);
+            }
+        }
         return false;
     }
 }
