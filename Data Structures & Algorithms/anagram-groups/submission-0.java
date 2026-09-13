4f0a35438f69443adf06b44e91f348bb1c2380cf
JOHN JOJI <johnjoji2222@gmail.com>
2026-09-13 07:29:08 +0530

    Update: anagram-groups - submission-0

--- Data Structures & Algorithms/anagram-groups/submission-0.java
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -1,31 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs.length == 0) {
-            return new ArrayList();
+        
+        Map<String, List<String>> map = new HashMap<>();
 
+        for(String str : strs) {
+
+            int[] charArray = new int[26];
+
+            for(int i = 0; i < str.length(); i++) {
+
+                charArray[str.charAt(i) - 'a']++;
+            }
+            String key = Arrays.toString(charArray);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
         }
-        Map<String, List> ansmap = new HashMap<>();
-
-        int [] count = new int[26];
-
-        for(String s : strs) {
-            Arrays.fill(count, 0);
-            for(char c : s.toCharArray()) {
-                count[c-'a']++;
-
-            }
-            StringBuilder sb = new StringBuilder("");
-
-            for(int i=0;i<26;i++) {
-                sb.append("#");
-                sb.append(count[i]);
-            }
-            String key = sb.toString();
-            if(!ansmap.containsKey(key)) {
-                ansmap.put(key,new ArrayList());
-            }
-            ansmap.get(key).add(s);
-        }
-        return new ArrayList(ansmap.values());
+        return new ArrayList<>(map.values());
     }
-}  
+}
