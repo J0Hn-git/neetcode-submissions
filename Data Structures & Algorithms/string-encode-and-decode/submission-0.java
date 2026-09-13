6c23070ef0951ff07a735faddeea61d7a683400d
JOHN JOJI <johnjoji2222@gmail.com>
2026-09-13 09:25:52 +0530

    Update: string-encode-and-decode - submission-0

--- Data Structures & Algorithms/string-encode-and-decode/submission-0.java
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.java
@@ -2,29 +2,37 @@ class Solution {
 
     public String encode(List<String> strs) {
 
-        if(strs.size()==0) {
-            return Character.toString((char)258);
-        }
-        String seperate = Character.toString((char)257);
         StringBuilder sb = new StringBuilder();
 
-        for(String str:strs) {
-            sb.append(str);
-            sb.append(seperate);
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
+        for(String str : strs) {
 
+            int strLen = str.length();
+            sb.append(strLen).append('#').append(str);
+        }
+        return sb.toString();
     }
 
     public List<String> decode(String str) {
 
-        if(str.equals(Character.toString((char)258))) {
-            return new ArrayList();
+        List<String> result = new ArrayList<>();
+
+        int i = 0;
+        while(i < str.length()){
+
+            int k = i;
+            while(str.charAt(i) != '#'){
+                i++;
+            }
+            String len = str.substring(k, i);
+            int strLen = Integer.parseInt(len);
+
+            i++;
+
+            String word = str.substring(i, i + strLen);
+            result.add(word);
+
+            i += strLen;
         }
-        String seperate = Character.toString((char)257);
-        return Arrays.asList(str.split(seperate,-1));
-
-
+        return result;
     }
 }
